d3993394d9791aa3a963158c5d5c85e7c6d9b24a
Viral Amin <vamin@mail.sfsu.edu>
2020-02-15 08:43:22 -0800

    crypto fix for new user signup

--- application/app/models/user.js
+++ application/app/models/user.js
@@ -42,7 +42,7 @@ module.exports = function(sequelize, DataTypes) {
                         return '';
                     }
 					salt = new Buffer(salt, 'base64');
-					return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+					return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
 				}
 			},
 			associate: function(models) {
