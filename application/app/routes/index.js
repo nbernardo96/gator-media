e5a7502a9fa7f65dc4e871c45563cfe9c4cdeedd
PakLikSiu <pakliksiu321@gmail.com>
2020-03-31 06:06:06 -0700

    added category function
    added category function

--- application/app/routes/index.js
+++ application/app/routes/index.js
@@ -2,11 +2,11 @@
 var mysql = require('mysql')
 var bodyParser = require('body-parser')
 
-
 module.exports = function(app) {
 // Home route
 	var index = require('../../app/controllers/index')
 	var items
+	var categories
 //app.get('/', index.render);
 	var db  = mysql.createConnection({
 		host: "database-2.cglovhs6vx6l.us-east-1.rds.amazonaws.com",
@@ -25,10 +25,15 @@ module.exports = function(app) {
 	db.query('SELECT * FROM sys.media_table', function (error, results, fields) {
 		items= results
 	});
+	db.query('SELECT DISTINCT category FROM sys.media_table', function (error, results, fields) {
+		categories= results
+	});
 
 	app.get('/', function(req, res, next){
 		res.render('index', {
 			item: items,
+			category: categories,
+			searchCate: "All categories",
 			title: ""
 		})
 	})
@@ -36,28 +41,35 @@ module.exports = function(app) {
 	app.use(bodyParser.urlencoded({extended:true}))
 	app.post('/', function(req, res, next){
 		var searchText = req.body.term;
-		console.log(searchText)
+		var searchCate = req.body.category;
+		console.log(searchText, searchCate)
 		if(req.body.term == null ||  req.body.term==""){
-			searchText = ""
-			res.redirect('/')
+			res.render('index', {
+				item: items,
+				category: categories,
+				searchCate: searchCate,
+				title: ""
+			})
+		} else {
+			db.query(`SELECT * FROM sys.media_table WHERE mediaName Like '%` + searchText + `%'`, function (error, results, fields) {
+				console.log(results.length) //debug statement
+				if (results.length == 0) {
+					res.render('index', {
+						item: items,
+						category: categories,
+						searchCate: "All categories",
+						title: ""
+					})
+				} else {
+					searchText = results[0].mediaName
+					res.render('index', {
+						item: results,
+						category: categories,
+						searchCate: searchCate,
+						title: ""
+					})
+				}
+			});
 		}
-		db.query(`SELECT * FROM sys.media_table WHERE mediaName Like '%` + searchText + `%'`, function (error, results, fields) {
-			console.log(results.length) //debug statement
-
-			if(results.length == 0){
-				res.render('index', {
-					item: items,
-					title: ""
-				})
-			} else {
-				searchText = results[0].mediaName
-				res.render('index', {
-					item: results,
-					title: ""
-				})
-			}
-		});
-
-
 	})
 };
